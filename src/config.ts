39174b4956af0311f43b2b8f9ba8d74fc7652aec
cosmicraw <rasyaadrian1234@gmail.com>
2025-02-04 18:09:58 +0700

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,52 @@
+export const SITE = {
+  website: "https://simple-portfolio.vicbox.dev", // replace this with your deployed domain
+  title: "Rasya Portfolio",
+  description: "A simple but beautiful portfolio created with Astro",
+  tags: ["portfolio", "Resume cv", "Astro"],
+  ogImage: "/og-image.webp",
+  logo: "logo",
+  logoText: "Portofolio",
+  lang: "en",
+  favicon: "/favicon.png",
+  repository: "https://github.com/vito8916/simple-portfolio.git",
+  author: "Rasya Adrian",
+  profile: "https://victoralvarado.dev/",
+};
+
+export const ME = {
+  name: "Rasya Adrian",
+  profession: "Web Developer",
+  profileImage: "rasya.png",
+  profileFacts: [
+    {
+      value: 2,
+      description: "Proyek",
+    },
+  ],
+  contactInfo: {
+    email: "vicbox.dev@vicbox.dev",
+    linkedin: "https://www.linkedin.com/in/victor-alvaradohn",
+    resumeDoc: "resume.pdf",
+  },
+  aboutMe:
+    "I am a software engineer with a passion for web development. I have experience in building web " +
+    "applications using modern technologies. I am a self-taught developer who enjoys learning new things and " +
+    "sharing knowledge with others.",
+};
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/vicbox.dev",
+    icon: "instagram-fill",
+    show: true,
+  },
+
+  {
+    name: "GitHub",
+    url: "https://github.com/vicbox.dev",
+    icon: "github-fill",
+    show: true,
+  },
+];
+
